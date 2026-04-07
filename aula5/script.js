520e55647dfced78f5ca2c285cf9980645f193da
gilberto <gilbertogulart70@gmail.com>
2026-04-07 12:15:56 -0300

    exercício

--- aula5/script.js
+++ aula5/script.js
@@ -1,8 +1,8 @@
-let num1 = parseFloat(prompt("Digite um número"))
-let num2 = parseFloat(prompt("Digite um número"))
+let num1 = parseFloat(prompt("Digite um número"));
+let num2 = parseFloat(prompt("Digite um número"));
 
 
-let result = num1 + num2
+let result = num1 + num2;
 console.log(result);
 
 alert("O resultado é igual a " + result);
