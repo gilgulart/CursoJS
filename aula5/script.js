b19649fd230c3501284091416276537ceb315658
gilberto <gilbertogulart70@gmail.com>
2026-04-07 07:54:04 -0300

    exercício

--- aula5/script.js
+++ aula5/script.js
@@ -1,3 +1,11 @@
-alert("Com a nossa mensagem")
-confirm("Deseja realmente pagar?")
-prompt("Digite uma frase")
+let num1 = prompt("Digite um número")
+let num2 = prompt("Digite outro número")
+
+num1 = parseFloat(num1)
+num2 = parseFloat(num2)
+
+let result = num1 + num2
+console.log(result);
+
+alert("O resultado é igual a " + result);
+    
