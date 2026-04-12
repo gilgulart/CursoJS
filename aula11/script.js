769bfcedf686da9ebeff6458b6cf03fda70c635f
gilberto <gilbertogulart70@gmail.com>
2026-04-12 20:20:04 -0300

    Tabela Verdade

---
+++ aula11/script.js
@@ -0,0 +1,34 @@
+/*
+Operadores Lógicos
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+
+// AND
+console.log("AND");
+
+console.log("Verdadeiro e Verdadeiro", true && true);
+console.log("Vedadeiro e Falso",true && false);
+console.log("Falso e verdadeiro",false && true);
+console.log("Falso e Falso", false && false);
+console.log("--------------------------------------");
+
+// OR
+console.log("OR");
+
+console.log("Verdadeiro e Verdadeiro", true || true);
+console.log("Vedadeiro e Falso",true || false);
+console.log("Falso e verdadeiro",false || true);
+console.log("Falso e Falso", false || false);
+
+console.log("--------------------------------------");
+
+// NOT
+console.log("NOT");
+
+console.log("Verdadeiro", ! true);
+console.log("Falso" , ! false);
+
+console.log("--------------------------------------");
+
