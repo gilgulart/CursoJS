390afc692e860ed0da4955deff49d0aef284dd98
gilberto <gilbertogulart70@gmail.com>
2026-04-15 12:24:50 -0300

    Arrays, Objetos e Loops

---
+++ aula13/exercicio/main.js
@@ -0,0 +1,6 @@
+const elements = [
+    {tag: 'p', text: 'Frase 1'},
+    {tag: 'div', text: 'Frase 2'},
+    {tag: 'footer', text: 'Frase 3'},
+    {tag: 'section', text: 'Frase 4'},
+]
---
+++ aula13/index.js
@@ -0,0 +1,14 @@
+for (let i = 0; i <= 20; i++) {
+ const isPeer = i % 2 === 0 ? 'par' : 'ímpar'  ; 
+    console.log(i, isPeer);
+    
+}
+
+const fruits = ["maçã", "pêra", "uva"];
+
+for (let index = 0; index < fruits.length; index++) {
+    const element = fruits[index];
+    console.log(`Índice ${index}: ${element}`);
+    
+    
+}
