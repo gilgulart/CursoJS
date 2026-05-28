3ff3ba3160f6463e3c9e019e4a5b893a42db505e
gilberto <gilbertogulart70@gmail.com>
2026-05-28 12:49:09 -0300

    arrays

---
+++ arrays/aula2.js
@@ -0,0 +1,9 @@
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+
+nomes.splice(-2, 1);
+console.log(nomes);
+
+const a1 = [1, 2, 3];
+const a2 = [4, 5, 6];
+const a3 = a1.concat(a2)
+console.log(a3);
