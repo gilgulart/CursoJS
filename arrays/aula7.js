723be042ed4e6320a1fdfcf57a59be9d6ad35b01
gilberto <gilbertogulart70@gmail.com>
2026-05-30 20:31:35 -0300

    arrays

--- arrays/aula7.js
+++ arrays/aula7.js
@@ -1,24 +1,11 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
 for (i in numeros){
-    console.log(num);
+    console.log(i);
 }
 
 for (num of numeros) {
     console.log(num);
 }
 
-const num = 5 ;
-let count = 0;
-
-for (let i = 1; i <= num; i++ ){
-    if (num % i === 0) count += 1;
-}
-
-if (count === 2) {
-    console.log("Número primo");
-    
-} else {
-    console.log("Não é primo");
-    
-}
+numeros.forEach(value => console.log(value))
