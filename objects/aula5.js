b3a71a57fb203670fd34853b824dfb965026aa3f
gilberto <gilbertogulart70@gmail.com>
2026-05-31 11:03:40 -0300

    objects

---
+++ objects/aula5.js
@@ -0,0 +1,25 @@
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.aumento = function (quantia) {this.preco += quantia}
+Produto.prototype.desconto = function (quantia) {this.preco -= quantia}
+
+function Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco);
+    this.cor = cor;
+
+}
+
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+Camiseta.prototype.aumento = function (percentual) {this.preco = this.preco + (this.preco * (percentual / 100))}
+
+const produto = new Produto('Gen', 150)
+const camiseta = new Camiseta('Regata', 7.5, 'Preta')
+camiseta.aumento(100)
+
+console.log(camiseta);
+console.log(produto);
+
