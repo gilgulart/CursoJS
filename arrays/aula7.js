4c75fdea8c947c70050643332645d3d8c3e08ae2
gilberto <gilbertogulart70@gmail.com>
2026-05-30 21:50:07 -0300

    arrays

--- arrays/aula7.js
+++ arrays/aula7.js
@@ -1,10 +1,10 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-for (i in numeros){
+for (let i in numeros){
     console.log(i);
 }
 
-for (num of numeros) {
+for (let num of numeros) {
     console.log(num);
 }
 
