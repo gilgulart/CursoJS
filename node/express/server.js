1dee09ac136ff8d2ba6ea9484a30f1f8473e2072
gilberto <gilbertogulart70@gmail.com>
2026-07-13 21:39:00 -0300

    template

--- node/express/server.js
+++ node/express/server.js
@@ -5,6 +5,8 @@ const path = require('path')
 
 app.use(express.urlencoded({extended: true}));
 
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
@@ -14,6 +16,5 @@ app.use(routes);
 app.listen(3000, () => {
     console.log('Acessar http://localhost:3000');
     console.log('Servidor executando na porta 3000');
-    
     
 });
