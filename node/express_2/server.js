2a92145863c3e05ec9e07165ebc75b9e44f4398d
gilberto <gilbertogulart70@gmail.com>
2026-07-15 22:39:37 -0300

    conectando MongoDB

---
+++ node/express_2/server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING)
+.then(() => {
+    console.log('MongoDB conectado');
+    app.emit('pronto')
+})
+.catch(err => {
+    console.error('Erro ao conectar mongoDB')
+    console.error(err)
+});
+
+const routes = require('./routes')
+const path = require('path')
+const myMiddleware = require('./src/middlewares/middleware.js')
+
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(myMiddleware)
+app.use(routes);
+
+app.on('pronto', () => {
+    
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000');
+        console.log('Servidor executando na porta 3000');
+        
+    })
+});
